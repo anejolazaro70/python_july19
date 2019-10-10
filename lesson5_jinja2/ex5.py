b197713846e466486512e6e81b717a83fdb49b20
Jose A. Lazaro <anejo.lazaro70@gmail.com>
2019-10-10 02:28:11 -0700

    Ejercicio 5 de lesson 5

---
+++ lesson5_jinja2/ex5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from jinja2 import FileSystemLoader, StrictUndefined
+from jinja2.environment import Environment
+
+env = Environment(undefined=StrictUndefined)
+env.loader = FileSystemLoader(['./templates'])
+
+template_file = 'cisco3.j2'
+template = env.get_template(template_file)
+output = template.render()
+print(output)
