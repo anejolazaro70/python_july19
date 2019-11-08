bcd182e775287b8e769b0598bfa99f25e73c0385
Jose A. Lazaro <anejo.lazaro70@gmail.com>
2019-11-08 10:56:07 -0800

    Ejercicios 1 (a, b, c) y 2 (a, b, c) de lesson Bonus REST-API

---
+++ bonus/restAPI/ex2a.py
@@ -0,0 +1,20 @@
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+from pprint import pprint
+
+
+def url_get(url, http_headers):
+    response = requests.get(url, headers=http_headers, verify=False)
+    return response
+
+def main():
+    requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+    url= 'https://netbox.lasthop.io/api/'
+    http_headers = {'Accept': 'application/json'}
+    response = url_get(url, http_headers)
+    pprint('HTTP status code {}'.format(response.status_code))
+    pprint(response.text)
+    pprint(response.headers)
+
+if __name__ == '__main__':
+    main()
